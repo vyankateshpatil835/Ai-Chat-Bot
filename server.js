bbc18c11e2d1243d913957794a57a417292b8e7c
Vyankatesh Patil <vyankateshpatil835@gmail.com>
2026-07-20 13:52:21 +0530

    Bug Fixed

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-app.get('/', (req, res) => {
-  res.send('AI Chat Assistant API is running');
-});
+// app.get('/', (req, res) => {
+//   res.send('AI Chat Assistant API is running');
+// });
 
 app.get('/api/health', (req, res) => {
   const state = mongoose.connection.readyState;
@@ -38,6 +38,12 @@ app.get('/api/health', (req, res) => {
 });
 app.use('/api/auth', authRoutes);
 app.use('/api/chats', chatRoutes);
+
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
